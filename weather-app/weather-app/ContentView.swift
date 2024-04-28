2137ec5da2bb7d220f8fcc22bfe3eee53e2693bc
Arman Nihal <armansaliknihal@gmail.com>
2024-04-28 18:15:28 +0300

    Initialize xcode project

---
+++ weather-app/weather-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather-app
+//
+//  Created by Arman Nihal on 28.4.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Arman!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Arman Nihal on 28.4.2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
