c200c46f518ebbfa2d88b6c39d2e079c52664319
Arman Nihal <armansaliknihal@gmail.com>
2024-05-04 08:12:13 +0300

    Structure model based on api response

--- weather-app/weather-app/HomePage/Entity.swift
+++ weather-app/weather-app/HomePage/Entity.swift
@@ -7,7 +7,44 @@
 
 import Foundation
 
-struct WeatherData: Codable {
-    let country: String
-    let weatherTempearture: Int
+struct WeatherData: Decodable {
+    var coord: CoordinatesResponse?
+    var weather: [WeatherResponse]?
+    var main: MainResponse?
+    var name: String?
+    var wind: WindResponse?
+    
+    init() {
+        self.coord = nil
+        self.weather = nil
+        self.main = nil
+        self.name = nil
+        self.wind = nil
+    }
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
 }
