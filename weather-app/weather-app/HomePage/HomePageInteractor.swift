d5f831683cbb3d380f7db830d09b4beaa7cfa7ea
Arman Nihal <armansaliknihal@gmail.com>
2024-05-23 18:52:18 +0300

    Move logic to the interactor side and start to use list of cities instead of one

--- weather-app/weather-app/HomePage/HomePageInteractor.swift
+++ weather-app/weather-app/HomePage/HomePageInteractor.swift
@@ -10,26 +10,29 @@ import Foundation
 protocol HomePageInteractorProtocol {
     //reference to the presenter
     var presenter: HomePagePresenterProtocol? { get set }
-    func getWeatherData(city: String)
+    func getWeatherData()
 }
 
 class HomePageInteractor: HomePageInteractorProtocol {
     var presenter: HomePagePresenterProtocol?
     var weatherService: WeatherAPIServiceProtocol
+    var cities: [String] = ["Helsinki"]
 
     init(weatherService: WeatherAPIServiceProtocol = URLSession.shared) {
         self.weatherService = weatherService
     }
     
-    func getWeatherData(city: String) {
-        self.weatherService.fetchWeatherDataFromAPI(city: city) { error, success, data in
-            if let error = error {
-                self.presenter?.didFetchWeatherData(with: .failure(error))
-                return
-            }
-            
-            if success, let weatherData = data {
-                self.presenter?.didFetchWeatherData(with: .success(weatherData))
+    func getWeatherData() {
+        for city in cities {
+            self.weatherService.fetchWeatherDataFromAPI(city: city) { error, success, data in
+                if let error = error {
+                    self.presenter?.didFetchWeatherData(with: .failure(error))
+                    return
+                }
+
+                if success, let weatherData = data {
+                    self.presenter?.didFetchWeatherData(with: .success(weatherData))
+                }
             }
         }
     }
