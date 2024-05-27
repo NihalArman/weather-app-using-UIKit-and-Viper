8ec82c5475c9ea48cd10c2fd8421599ce12a978a
Arman Nihal <armansaliknihal@gmail.com>
2024-05-27 13:16:28 +0300

    Show detail weather information

--- weather-app/weather-app/DetailPage/DetailPageViewController.swift
+++ weather-app/weather-app/DetailPage/DetailPageViewController.swift
@@ -33,14 +33,18 @@ class DetailPageViewController: UIViewController, DetailPageViewControllerProtoc
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        let rowsAvailable = getTableData().count
+        return rowsAvailable
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell()
 
         cell.imageView?.image = UIImage(named: "helsinki")
-        cell.textLabel?.text = "Table View initialized: "
+
+        let rowToPresent: [String] = getTableData()
+        cell.imageView?.image = UIImage(systemName: "cloud.sun.fill")
+        cell.textLabel?.text = rowToPresent[indexPath.row]
         return cell
     }
 
@@ -50,5 +54,27 @@ class DetailPageViewController: UIViewController, DetailPageViewControllerProtoc
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-}
 
+    private func getTableData() -> [String] {
+        let temparatureInCelsius: Double = (weatherDetailData.main?.temp ?? 0.0) - 273.15
+        let temparature: String = "Temparature: " + String(format: "%.1f", temparatureInCelsius)
+
+        let feelsLikeInCelsius: Double = (weatherDetailData.main?.feels_like ?? 0.0) - 273.15
+        let feelsLike: String = "Feels like: " + String(format: "%.1f", feelsLikeInCelsius)
+
+        let maxTempInCelsius: Double = (weatherDetailData.main?.temp_max ?? 0.0) - 273.15
+        let maxTemp: String = "Maximum Temparature: " + String(format: "%.1f", maxTempInCelsius)
+
+        let minTempInCelsius: Double = (weatherDetailData.main?.temp_min ?? 0.0) - 273.15
+        let minTemp: String = "Minimum Temparature: " + String(format: "%.1f", minTempInCelsius)
+
+        let humidity: String = "Humidity: " + String(format: "%.1f", weatherDetailData.main?.humidity ?? "Humidity not available")
+
+        let pressure: String = "Pressure: " + String(format: "%.1f", weatherDetailData.main?.pressure ?? "Pressure not available")
+
+
+        var weatherInformations: [String] = [temparature, feelsLike, maxTemp, minTemp, humidity, pressure]
+
+        return weatherInformations
+    }
+}
