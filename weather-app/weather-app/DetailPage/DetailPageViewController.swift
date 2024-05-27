9075a33272170e0c4bed9a7e875b97e4564f5ff5
Arman Nihal <armansaliknihal@gmail.com>
2024-05-27 12:29:49 +0300

    Initialize table view in the detail page

--- weather-app/weather-app/DetailPage/DetailPageViewController.swift
+++ weather-app/weather-app/DetailPage/DetailPageViewController.swift
@@ -12,22 +12,42 @@ protocol DetailPageViewControllerProtocol {
     var presenter: DetailPagePresenterProtocol? { get set }
 }
 
-class DetailPageViewController: UIViewController, DetailPageViewControllerProtocol {
-
+class DetailPageViewController: UIViewController, DetailPageViewControllerProtocol, UITableViewDelegate, UITableViewDataSource {
     var presenter: DetailPagePresenterProtocol?
-
     var weatherDetailData: WeatherData = WeatherData()
+
+    let tableView: UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
-
         weatherDetailData = presenter!.getDetailInformation()
-
         self.navigationItem.title = self.weatherDetailData.name
-        let cityImage = UIImage(named: "helsinki")
-        let imageView = UIImageView(image: cityImage)
-        imageView.frame = CGRect(x: 0, y: 100, width: 400, height: 200)
-        self.view.addSubview(imageView)
+
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        activateTableViewConstraints()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+
+        cell.imageView?.image = UIImage(named: "helsinki")
+        cell.textLabel?.text = "Table View initialized: "
+        return cell
+    }
+
+    private func activateTableViewConstraints() {
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 }
+
